bfc3e06879947a8b30f0ba96dd93a40557030a60
Inna-kv27 <inna.kvitka27@gmail.com>
2025-07-07 10:10:08 +0200

    fix: Correct module import path case for Contact model

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import Contact from '../models/Contact.js';
+import Contact from '../models/contact.js';
 import cloudinary from '../utils/cloudinary/cloudinary.js'; // Імпортуємо налаштований Cloudinary
 
 export const listContacts = async (
